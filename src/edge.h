d7085b82e191a5067e9e68c76eb8dd8da9aff53f
zbyju <zbynek.jurica@gmail.com>
2022-02-22 14:31:51 +0100

    Add basic properties to classes

--- src/edge.h
+++ src/edge.h
@@ -1,8 +1,16 @@
 #ifndef EDGE_H
 #define EDGE_H
 
+#include <memory>
+
+#include "node.h"
+
 class Edge {
  private:
+  std::shared_ptr<Node> from;
+  std::shared_ptr<Node> to;
+  uint32_t weight;
+
  public:
 };
 
--- src/node.h
+++ src/node.h
@@ -1,8 +1,16 @@
 #ifndef NODE_H
 #define NODE_H
 
+#include <memory>
+#include <vector>
+
+#include "edge.h"
+
 class Node {
  private:
+  uint8_t id;
+  std::vector<std::shared_ptr<Edge>> edges;
+
  public:
 };
 
