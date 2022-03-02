b715e9f18102c39d9985b17b75d771062fc96f51
zbyju <zbynek.jurica@gmail.com>
2022-03-02 22:06:57 +0100

    Add comments

--- src/main.cpp
+++ src/main.cpp
@@ -16,12 +16,80 @@ enum EdgeState { s_undefined, included, excluded };
 enum NodeColor { c_undefined, red, green };
 using Edge = tuple<uint8_t, uint8_t, unsigned int, EdgeState>;
 
+/**
+ * @brief Prints adjacency matrix.
+ *
+ * @param n - number of nodes
+ * @param edges - vector of edges with weights
+ */
+void printGraph(unsigned int n, vector<Edge> &edges) {
+  string res = "";
+  unsigned int maxWeight = 0;
+  for (auto &e : edges) {
+    if (get<2>(e) > maxWeight) maxWeight = get<2>(e);
+  }
+  unsigned int maxLength = to_string(maxWeight).length();
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
+      auto edge = findEdgeByIds(edges, i, j);
+      string weight = "0";
+      if (edge != nullptr) weight = to_string(get<2>(*edge));
+      string padding = " ";
+      while (padding.length() + weight.length() < (unsigned)maxLength + 1) {
+        padding += " ";
+      }
+      res += weight + padding;
+    }
+    res += "\n";
+  }
+  cout << res << endl;
+}
+
+/**
+ * @brief Prints weights of all the edges in the order that they are stored in.
+ *
+ * @param edges - vector of edges to be printed
+ */
+void printEdgeWeights(vector<Edge> &edges) {
+  for (auto &e : edges) {
+    cout << get<2>(e) << endl;
+  }
+}
+
+/**
+ * @brief Prints weight, time and # of recursion calls.
+ *
+ * @param time
+ */
+void printResult(double time) {
+  cout << endl << "=========================" << endl;
+  cout << "Max weight: " << maxWeight << endl;
+  cout << "Calculation time: " << time << endl;
+  cout << "Recursion call count: " << recursionCount << endl;
+}
+
+/**
+ * @brief Parses args from terminal.
+ *
+ * @param argc - number of arguments
+ * @param argv - array of arguments
+ * @return string - returns file path to the input file
+ */
 string parseArgs(int argc, char *argv[]) {
   if (argc != 3) return "";
   if (strcmp(argv[1], "-f") != 0) return "";
   return argv[2];
 }
 
+/**
+ * @brief Finds an edge in the vector based on id's. It ignores order of id's
+ * (symmetrical find).
+ *
+ * @param edges - vector of edges
+ * @param id1 - id of one of the nodes
+ * @param id2 - id of the other node
+ * @return unique_ptr<Edge> - reference to the edge
+ */
 unique_ptr<Edge> findEdgeByIds(vector<Edge> edges, uint8_t id1, uint8_t id2) {
   for (auto &e : edges) {
     if (get<0>(e) == id1 && get<1>(e) == id2) return make_unique<Edge>(e);
@@ -30,6 +98,14 @@ unique_ptr<Edge> findEdgeByIds(vector<Edge> edges, uint8_t id1, uint8_t id2) {
   return nullptr;
 }
 
+/**
+ * @brief Goes through an input file and returns number of nodes.
+ * It also adds all the edges (structure of the graph) into the provided vector.
+ *
+ * @param inputPath - string representing the path to the input file
+ * @param edges - vector for adding all the edges to
+ * @return unsigned int n - returns number of nodes
+ */
 unsigned int parseFile(string &inputPath, vector<Edge> &edges) {
   ifstream infile(inputPath);
   string line;
@@ -56,43 +132,30 @@ unsigned int parseFile(string &inputPath, vector<Edge> &edges) {
   return n;
 }
 
-void printGraph(unsigned int n, vector<Edge> &edges) {
-  string res = "";
-  unsigned int maxWeight = 0;
-  for (auto &e : edges) {
-    if (get<2>(e) > maxWeight) maxWeight = get<2>(e);
-  }
-  unsigned int maxLength = to_string(maxWeight).length();
-  for (int i = 0; i < n; ++i) {
-    for (int j = 0; j < n; ++j) {
-      auto edge = findEdgeByIds(edges, i, j);
-      string weight = "0";
-      if (edge != nullptr) weight = to_string(get<2>(*edge));
-      string padding = " ";
-      while (padding.length() + weight.length() < (unsigned)maxLength + 1) {
-        padding += " ";
-      }
-      res += weight + padding;
-    }
-    res += "\n";
-  }
-  cout << res << endl;
-}
-
-void printEdgeWeights(vector<Edge> &edges) {
-  for (auto &e : edges) {
-    cout << get<2>(e) << endl;
-  }
-}
-
+/**
+ * @brief Takes two timestamps and returns time difference in seconds.
+ *
+ * @param start - start time
+ * @param end - end time
+ * @return double - time in seconds
+ */
 double calculateTime(clock_t &start, clock_t &end) {
   return double(end - start) / CLOCKS_PER_SEC;
 }
 
+/**
+ * @brief Function for sorting edges by weight (highest weight first).
+ */
 bool sortByWeight(const Edge &e1, const Edge &e2) {
   return get<2>(e1) > get<2>(e2);
 }
 
+/**
+ * @brief Sums up all the weights of edges that were included in the subgraph.
+ *
+ * @param edges - vector of edges
+ * @return unsigned int - total weight of all included edges
+ */
 unsigned int getChosenWeight(const vector<Edge> &edges) {
   unsigned int sum = 0;
   for (auto &e : edges) {
@@ -101,6 +164,13 @@ unsigned int getChosenWeight(const vector<Edge> &edges) {
   return sum;
 }
 
+/**
+ * @brief Sums up all the weights of edges that were not excluded from the
+ * subgraph.
+ *
+ * @param edges - vector of edges
+ * @return unsigned int - total weight of all included/undefined edges.
+ */
 unsigned int getPotentialWeight(const vector<Edge> &edges) {
   unsigned int sum = 0;
   for (auto &e : edges) {
@@ -109,20 +179,46 @@ unsigned int getPotentialWeight(const vector<Edge> &edges) {
   return sum;
 }
 
+/**
+ * @brief Checks if the edge can be colored Red - Green or if there would be a
+ * conflict.
+ */
 bool canColorRedGreen(const vector<NodeColor> &colors, const Edge &edge) {
   return colors[get<0>(edge)] != green && colors[get<1>(edge)] != red;
 }
 
+/**
+ * @brief Checks if the edge can be colored Green - Red or if there would be a
+ * conflict.
+ */
 bool canColorGreenRed(const vector<NodeColor> &colors, const Edge &edge) {
   return colors[get<0>(edge)] != red && colors[get<1>(edge)] != green;
 }
 
+/**
+ * @brief Checks if it makes sense to not include this edge - if both nodes have
+ * a non-matching color then not including this edge would be strictly worse
+ * than including it.
+ */
 bool shouldTryNotAdding(const vector<NodeColor> &colors, const Edge &edge) {
   NodeColor c1 = colors[get<0>(edge)];
   NodeColor c2 = colors[get<1>(edge)];
   return !((c1 == red && c2 == green) || (c1 == green && c2 == red));
 }
 
+/**
+ * @brief The main recursive function that checks all the possible subgraphs and
+ * finds the max weight.
+ *
+ * @param colors - vector of colors of the nodes (i-th position = color of the
+ * i'th node)
+ * @param edges - vector of edges sorted from the heaviest to the lightest.
+ * @param index - index of currently checked edge
+ * @param chosenWeight - total weight of all included edges (this is for
+ * optimization)
+ * @param potentialWeight - total potential weight of all edges (this is for
+ * optimization)
+ */
 void solveDFS(vector<NodeColor> &colors, vector<Edge> &edges,
               unsigned int index, unsigned int chosenWeight,
               unsigned int potentialWeight) {
@@ -135,7 +231,8 @@ void solveDFS(vector<NodeColor> &colors, vector<Edge> &edges,
   Edge nextEdge = edges.at(index);
   get<3>(nextEdge) = included;
   edges[index] = nextEdge;
-  // Try to color nodes Red - Green
+
+  // Try to include this edge and color it's nodes Red - Green
   if (canColorRedGreen(colors, nextEdge)) {
     vector<NodeColor> redgreen(colors);
     redgreen[get<0>(nextEdge)] = red;
@@ -144,6 +241,7 @@ void solveDFS(vector<NodeColor> &colors, vector<Edge> &edges,
              potentialWeight);
   }
 
+  // Try to include this edge and color it's nodes Green - Red
   if (canColorGreenRed(colors, nextEdge)) {
     vector<NodeColor> greenred(colors);
     greenred[get<0>(nextEdge)] = green;
@@ -152,6 +250,7 @@ void solveDFS(vector<NodeColor> &colors, vector<Edge> &edges,
              potentialWeight);
   }
 
+  // Try not to include this edge at all
   if (shouldTryNotAdding(colors, nextEdge)) {
     get<3>(nextEdge) = excluded;
     edges[index] = nextEdge;
@@ -160,6 +259,14 @@ void solveDFS(vector<NodeColor> &colors, vector<Edge> &edges,
   }
 }
 
+/**
+ * @brief Wrapper function that sets up the calculation and analytics.
+ *
+ * @param n - number of nodes
+ * @param edges - vector of edges
+ * @param colors - vector of colors of nodes
+ * @return unsigned int - max weight
+ */
 unsigned int solve(unsigned int n, vector<Edge> &edges,
                    vector<NodeColor> &colors) {
   clock_t calculationStart = clock();
@@ -175,14 +282,17 @@ unsigned int solve(unsigned int n, vector<Edge> &edges,
 
   calculationEnd = clock();
 
-  cout << endl << "=========================" << endl;
-  cout << "Max weight: " << maxWeight << endl;
-  cout << "Calculation time: "
-       << calculateTime(calculationStart, calculationEnd) << endl;
-  cout << "Recursion call count: " << recursionCount << endl;
+  printResult(calculateTime(calculationStart, calculationEnd));
   return maxWeight;
 }
 
+/**
+ * @brief Main function - gets all inputs, prints outputs.
+ *
+ * @param argc - number of arguments
+ * @param argv - array of arguments
+ * @return int - status code
+ */
 int main(int argc, char *argv[]) {
   string inputPath = parseArgs(argc, argv);
   if (inputPath == "") return 1;
